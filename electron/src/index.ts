866cb5aec8b8a64be624c26d84861a1b3375a9b9
FreeIntelligence <free04381@gmail.com>
2021-12-04 00:38:46 -0300

    Se desactivo el sistema de actualizaciones automaticas.

--- electron/src/index.ts
+++ electron/src/index.ts
@@ -7,7 +7,7 @@ import type { MenuItemConstructorOptions } from 'electron';
 import { app, MenuItem } from 'electron';
 import electronIsDev from 'electron-is-dev';
 import unhandled from 'electron-unhandled';
-import { autoUpdater } from 'electron-updater';
+//import { autoUpdater } from 'electron-updater';
 
 import {
   ElectronCapacitorApp,
@@ -62,7 +62,7 @@ if (electronIsDev) {
   // Initialize our app, build windows, and load content.
   await myCapacitorApp.init();
   // Check for updates if we are in a packaged app.
-  autoUpdater.checkForUpdatesAndNotify();
+  //autoUpdater.checkForUpdatesAndNotify();
 })();
 
 // Handle when all of our windows are close (platforms have their own expectations).
