7732a00b95b00fd60ee33345968f513915151db6
FreeIntelligence <free04381@gmail.com>
2021-12-05 07:17:57 -0300

    Avances en el boceto para la configuracion del sistema.

--- src/app/configuration/home/home.page.ts
+++ src/app/configuration/home/home.page.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
+import { IonItem } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage implements OnInit, AfterViewInit {
+
+  @ViewChildren(IonItem) items: QueryList<IonItem>;
 
   constructor() { }
 
   ngOnInit() {
+    
   }
+
+  ngAfterViewInit() {
+    console.log(this.items);
+  }
+
 
 }
