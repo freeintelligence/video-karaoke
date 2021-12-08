bc04294482d5dfa044d5dca920a824c5f151bc8e
FreeIntelligence <free04381@gmail.com>
2021-12-08 10:49:21 -0300

    Ahora la configuracion se guarda en la base de datos.

--- electron/src/events/config-settings.ts
+++ electron/src/events/config-settings.ts
@@ -9,6 +9,16 @@ export class ConfigSettings {
 
   async run() {
     await this.isConfigSetted();
+    await this.setConfig();
+  }
+
+  async setConfig() {
+    ipcMain.on('set-config', async (event, config) => {
+      const data = await Config.get();
+      config.firstConfig = true;
+      await data.update(config);
+      event.reply('config-setted', data);
+    });
   }
 
   async isConfigSetted() {
