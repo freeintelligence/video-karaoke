1bfc20a7dcf0676c004011d51e19bb97ea78a2df
FreeIntelligence <free04381@gmail.com>
2021-12-09 05:12:17 -0300

    Detalle en las rutas.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,15 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { ConfigSettedGuard } from 'src/guards/config-setted.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'music', pathMatch: 'full' },
+  {
+    path: '',
+    children: [
+      { path: 'music', loadChildren: () => import('./music/music.module').then(m => m.MusicModule) },
+      { path: '', redirectTo: 'music', pathMatch: 'full' },
+    ],
+    canActivate: [ ConfigSettedGuard ],
+  },
   { path: 'config', loadChildren: () => import('./config/config.module').then(m => m.ConfigModule) },
-  { path: 'music', loadChildren: () => import('./music/music.module').then(m => m.MusicModule), canActivate: [ ConfigSettedGuard ] },
 ];
 
 @NgModule({
