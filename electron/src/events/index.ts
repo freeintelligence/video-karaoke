9325b306c4f92ca461f33e631da5010e97a212a2
FreeIntelligence <free04381@gmail.com>
2021-12-09 06:33:42 -0300

    Se crean instancias de todos los eventos para que se ejecuten y comiencen a escuchar.

--- electron/src/events/index.ts
+++ electron/src/events/index.ts
@@ -1,9 +1,13 @@
+import { ArtistEvents } from './artist-events';
 import { ConfigSettings } from './config-settings';
+import { GenreEvents } from './genre-events';
 import { MediaEvents } from './media-events';
 
 export async function mainEvents() {
 
   const configSettings = new ConfigSettings();
   const mediaEvents = new MediaEvents();
+  const artistEvents = new ArtistEvents();
+  const genreEvents = new GenreEvents();
 
 }
