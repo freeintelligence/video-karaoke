df184837e501770d616dd864c3508c9160d85423
FreeIntelligence <free04381@gmail.com>
2021-12-09 06:32:39 -0300

    Ahora, el spinner para mostrar carga de los generos solo se muestra segun cierta condicion.

--- src/app/music/list/list.page.ts
+++ src/app/music/list/list.page.ts
@@ -8,36 +8,16 @@ import { MediaService } from 'src/services/media.service';
 })
 export class ListPage implements OnInit {
 
+  genreLoading: boolean = true;
   genreList: any[] = [];
+  artistLoading: boolean = true;
   artistList: any[] = [];
+  songLoading: boolean = true;
   songList: any[] = [];
 
   constructor(private mediaService: MediaService) {}
 
   ngOnInit() {
-    this.genreList = [
-      { id: 0, name: 'Reggae', image: 'https://cdn.quonomy.com/3485/conversions/17-sorprendentes-curiosidades-sobre-la-musica-reggae-h-lg.jpg' },
-      { id: 1, name: 'Rock', image: 'https://www.poblanerias.com/wp-content/archivos/2020/07/Guitarra-1021x580.jpg' },
-      { id: 2, name: 'Reggaeton', image: 'https://www.cinconoticias.com/wp-content/uploads/reggaeton-696x392.jpg' },
-      { id: 3, name: 'Clasica', image: 'https://i.ytimg.com/vi/dhzIMZvYgaA/maxresdefault.jpg' },
-    ];
-
-    this.artistList = [
-      { id: 0, name: 'The Wailers', image: 'https://www.lifeder.com/wp-content/uploads/2017/03/wailers-reggae.jpg' },
-      { id: 1, name: 'Bob Marley', image: 'https://www.lifeder.com/wp-content/uploads/2017/03/bob-marley.jpg' },
-      { id: 2, name: 'Peter Tosh', image: 'https://www.lifeder.com/wp-content/uploads/2020/02/347px-PeterToshWithRobbieShakespeare1978-304x420.jpg' },
-      { id: 3, name: 'Bunny Wailer', image: 'https://www.lifeder.com/wp-content/uploads/2020/02/640px-Bunny-Wailer-Smile-Jamaica-2008.jpg' },
-      { id: 4, name: 'Toots and the Maytals', image: 'https://www.lifeder.com/wp-content/uploads/2017/03/toots-and-the-maytals-2.jpg' },
-      { id: 5, name: 'Black Uhuru', image: 'https://www.lifeder.com/wp-content/uploads/2020/02/Black-Uhuru-631x420.jpg' },
-      { id: 6, name: 'UB40', image: 'https://www.lifeder.com/wp-content/uploads/2020/02/640px-UB40_feat._Ali_Campbell_Astro__Mickey_Virtue_-_2018174193650_2018-06-23_Rock_the_Ring_-_1D_X_MK_II_-_1061_-_AK8I6742-630x420.jpg' },
-      { id: 7, name: 'Inner Circle', image: 'https://www.lifeder.com/wp-content/uploads/2017/03/inner-circle-banda-reggaelifederimagen.jpg' },
-      { id: 8, name: 'Jimmy Cliff', image: 'https://www.lifeder.com/wp-content/uploads/2020/02/640px-Jimmy_Cliff_-_Festival_du_Bout_du_Monde_2012_-_022-630x420.jpg' },
-      { id: 9, name: 'The Abyssianians', image: 'https://www.lifeder.com/wp-content/uploads/2020/02/640px-The_Abyssinians_LanzamientoRototomReggaeContestLatino10-11_20101015-630x420.jpg' },
-      { id: 10, name: 'Alpha Blondy', image: 'https://www.lifeder.com/wp-content/uploads/2020/02/640px-Alpha_Blondy_2007.07.12_002-628x420.jpg' },
-      { id: 11, name: 'Gregory Isaacs', image: 'https://www.lifeder.com/wp-content/uploads/2020/02/321px-Gregory_Isaacs_SNWMF_2010_1_-_on_stage-2-281x420.jpg' },
-    ];
-
-    this.songList = Array.from({ length: 20 }).map((val, i) => ({ id: i, name: 'Canción #' + i }));
   }
 
 }
