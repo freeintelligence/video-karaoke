ee3cebecc7a8819e66b6e29a712ef29636152c60
FreeIntelligence <free04381@gmail.com>
2021-12-11 22:52:52 -0300

    Compatibilidad con navegador para probar..

---
+++ src/services/browser-data/config.ts
@@ -0,0 +1,12 @@
+export default {
+    id: 1,
+    firstConfig: true,
+    genreSearch: true,
+    artistSearch: true,
+    buttonLeft: "ArrowLeft",
+    buttonRight: "ArrowRight",
+    buttonUp: "ArrowUp",
+    buttonDown: "ArrowDown",
+    buttonEnter: "Enter",
+    buttonBack: "Backspace"
+};
--- src/services/config.service.ts
+++ src/services/config.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ElectronService } from 'src/app/electron.service';
+import browserDataConfig from './browser-data/config';
 
 @Injectable({
   providedIn: 'root'
@@ -15,16 +16,23 @@ export class ConfigService {
 
   async getConfig(): Promise<any> {
     return new Promise((resolve, reject) => {
-      const timeout = setTimeout(() => reject(new Error('timeout')), 2000);
+      if (this.electron.isElectronApp) {
+        // Electron App
+        const timeout = setTimeout(() => reject(new Error('timeout')), 2000);
 
-      this.electron.ipcRenderer.send('is-config-setted');
-      this.electron.ipcRenderer.once('config-is-setted', (event, config) => {
-        this.lastConfig = config;
-        if (timeout) {
-          clearTimeout(timeout);
-        }
-        return resolve(config);
-      });
+        this.electron.ipcRenderer.send('is-config-setted');
+        this.electron.ipcRenderer.once('config-is-setted', (event, config) => {
+          this.lastConfig = config;
+          if (timeout) {
+            clearTimeout(timeout);
+          }
+          return resolve(config);
+        });
+      } else {
+        // Browser App
+        this.lastConfig = browserDataConfig;
+        return resolve(browserDataConfig);
+      }
     });
   }
 
